ab00092ae64a729d45454caa278c2641742bb997
Nicolas Avila <nicolasavila@gmail.com>
2013-12-02 03:16:21 -0600

    working express

--- web.js
+++ web.js
@@ -1,35 +1,44 @@
 var express = require("express");
-var app = express();
+//var app = express();
 var fs = require('fs');
 var mimeLookup = require('mime');
 
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-
-            var rootPath = '.',
-                filePath = rootPath+request.url;
-
-            fs.exists(filePath, function(exists) {
-
-            if (exists) {
-                fs.readFile(filePath, function(error, content) {
-                    if (error) {
-                        response.send(500);
-                    }
-                    else {
-                        var contentType = mimeLookup.lookup(filePath);
-                        response.set('Content-Type',contentType)
-                        response.send(200,content);
-                    }
-                });
-            }
-            else {
-                response.send(200, filePath);
-            }
-});
+//app.use(express.logger());
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+express.createServer(function (request, response) {
+//app.get('/', function(request, response) {
+
+    console.log('trigger '+ request.url);
+    var rootPath = '.',
+        filePath = rootPath+request.url;
+
+    fs.exists(filePath, function(exists) {
+        console.log('exists '+ filePath);
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.send(500);
+                }
+                else {
+                    var contentType = mimeLookup.lookup(filePath);
+                    // response.set('Content-Type',contentType);
+                    // response.send(200,content);
+
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.send(200, filePath);
+        }
+    });
+}).listen(port);
+console.log("Listening on " + port);
+
+// var port = process.env.PORT || 5000;
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
