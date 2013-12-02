85d5b83160abefbbedfee55ccb0ef6fd420e6b8f
Nicolas Avila <nicolasavila@gmail.com>
2013-12-02 02:50:43 -0600

    improved express server

--- web.js
+++ web.js
@@ -1,9 +1,32 @@
 var express = require("express");
 var app = express();
+var fs = require('fs');
+var mimeLookup = require('mime');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+            var rootPath = '.',
+                filePath = rootPath+request.url;
+
+            fs.exists(filePath, function(exists) {
+
+            if (exists) {
+                fs.readFile(filePath, function(error, content) {
+                    if (error) {
+                        response.send(500);
+                    }
+                    else {
+                        var contentType = mimeLookup.lookup(filePath);
+                        response.set('Content-Type',contentType)
+                        response.send(200,content);
+                    }
+                });
+            }
+            else {
+                response.send(200, filePath);
+            }
 });
 
 var port = process.env.PORT || 5000;
